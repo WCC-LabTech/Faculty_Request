3329573fa29349675e62f423a2dbb11bbb1fdd98
Jallal <jhazzat@gmail.com>
2013-06-22 16:00:04 -0400

    data base views separation

--- project/faculty_request/views.py
+++ project/faculty_request/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.contrib.auth.models import User,Group
+from rest_framework import viewsets
+from faculty_request.serializers import faculty_Serializer,admin_Serializer,labtech_Serializer
+from faculty_request.models import request
+
+class faculty_view(viewsets.ModelViewSet):
+	queryset=request.objects.all()
+	serializer_class=faculty_Serializer
+
+
+
+class admin_view(viewsets.ModelViewSet):
+        queryset=request.objects.all()
+        serializer_class=admin_Serializer
+
+
+
+class labtech_view(viewsets.ModelViewSet):
+        queryset=request.objects.all()
+        serializer_class=labtech_Serializer
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from rest_framework import routers
+from faculty_request import views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+#router =routers.DefaultRouter()
+#router.register(r'request',views.faculty_viewall)
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,4 +17,10 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),)
+    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^',include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls',namespace='rest_framework')),
+    #url(r'^request/$',views.faculty_viewall.as_view()),
+    url(r'^request/',include('faculty_request.urls')),
+    
+)
