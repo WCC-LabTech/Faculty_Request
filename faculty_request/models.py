44d82c9c7bf6a8ea46945be189299e3a0e2f671c
Jallal <jhazzat@gmail.com>
2013-08-20 14:19:13 -0400

    Add update function

---
+++ faculty_request/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+ROOM='Room'
+EQUIPMENT= 'Equipment'
+CONFIGURE='Configure'
+LABS='Labs'
+OTHER='Other'
+PENDING='Pending'
+APPROVED='Approved'
+DELEGATED='Delegated'
+COMPLETED='Completed'
+
+
+request_type= (
+    (ROOM,'Room'),
+    (EQUIPMENT, 'Equipment'),
+    (CONFIGURE,'Configure'),
+    (LABS,'Labs'),
+    (OTHER,'Other'),
+)
+request_status=(
+    (PENDING,'Pending'),
+    (APPROVED,'Approved'),
+    (DELEGATED,'Delegated'),
+    (COMPLETED,'Completed'),
+)
+class requests (models.Model):
+    faculty_Name= models.ForeignKey(User,related_name='+')
+    labtech_Name=models.ForeignKey(User,related_name='+',blank=True,null=True,)
+    uploaded= models.FileField(upload_to='documents/%Y/%m/%d', blank=True,)
+    subject=models.CharField(max_length=100)
+    description=models.TextField()
+    issued_date=models.DateTimeField(auto_now_add=True)
+    due_date=models.DateField()
+    request_Type=models.CharField(max_length=20, choices=request_type, default=OTHER)
+    request_status=models.CharField(max_length=20,choices=request_status,default=PENDING)     
+
+    def __unicode__(self):
+        return unicode(self.subject)
+# Create your models here.
---
+++ faculty_request/views.py
@@ -0,0 +1,96 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import simplejson
+from django.http import HttpResponse
+from django.contrib.auth.models import User,Group
+#from rest_framework import viewsets
+#from faculty_request.serializers import faculty_Serializer,admin_Serializer,labtech_Serializer
+from faculty_request import models
+from django.http import HttpResponse
+from django.core import serializers
+from django.views.decorators.http import require_http_methods 
+
+'''class faculty_view(viewsets.ModelViewSet):
+	queryset=request.objects.all()
+	serializer_class=faculty_Serializer
+
+
+
+class admin_view(viewsets.ModelViewSet):
+        queryset=request.objects.all()
+        serializer_class=admin_Serializer
+
+
+
+
+class labtech_view(viewsets.ModelViewSet):
+        queryset=request.objects.all()
+        serializer_class=labtech_Serializer;'''
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def request_save(request):
+    post = request.POST
+    requests = models.requests(
+            faculty_Name = User.objects.get(id=post["faculty_Name"]),
+            labtech_Name = User.objects.get(id=post['labtech_Name']),
+            subject = post['subject'],
+            description = post['description'],
+            due_date = post['due_date'],
+            request_Type = post['request_Type'],
+            request_status = 'Pending',
+    )
+    try:
+        requests.save()
+        data = {'data': 'Request Created'}
+        code = 201
+    except:
+        data = {'message': 'Request not saved'}
+        code = 400
+    return HttpResponse(simplejson.dumps(data), status=code)
+
+
+def admin_view(request):
+	faculty= models.requests.objects.all()
+	data = serializers.serialize('json', faculty)
+	return HttpResponse(data, mimetype = 'application/json')
+
+
+
+
+def request_update(request):
+	post = request.POST
+	requests= models.requests.objects.all(id=post['id'])
+	requests.labtech_Name=User.objects.get(id=post["labtech_Name"])
+	requests.request_status=post['request_status']
+
+
+	try:
+		requests.save()
+		data={'data': 'Request Updated'}
+		code=200
+
+	except:
+		data={'message':'request not updated'}
+		code=400
+	return HttpResponse(simplejson.dumps(data), status=code)
+		
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
